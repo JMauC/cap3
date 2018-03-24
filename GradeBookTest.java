73501fd4569655484b280242efc4ebf47ddf32ab
JMauC <jmaunc@outlook.com>
2018-03-24 12:43:28 -0300

    "Reorganização" do projeto com o Eclipse.

--- GradeBookTest.java
+++ GradeBookTest.java
@@ -4,9 +4,9 @@ public class GradeBookTest{
     public static void main(  String[] args ){
         //Criação dos objetos GradeBook
         GradeBook gradeBook1 = new GradeBook(
-            "CS101 Introduction to Java Programming");
+            "CS101 Introduction to Java Programming", "Matt");
         GradeBook gradeBook2 = new GradeBook(
-            "CS102 Data Structures in Java");
+            "CS102 Data Structures in Java", "John");
         
         //Exibe os valores iniciais de cada objeto
         System.out.printf("gradebook1 course name is: %s\n", 
